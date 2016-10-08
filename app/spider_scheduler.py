516df11858021d5f312204a7a3e9cb6550811882
XBZX <liwei429>
2016-10-08 14:19:16 +0800

    2016-10-8 2.4

--- app/spider_scheduler.py
+++ app/spider_scheduler.py
@@ -6,7 +6,9 @@ import time
 from apscheduler.schedulers.background import BackgroundScheduler
 from apscheduler.schedulers.blocking import BlockingScheduler
 
+
 def scrapyd_curl():
+
     print('Tick! The time is: %s' % time.strftime('%Y-%m-%d %X',time.localtime()))
 
 def run_interval():
@@ -17,20 +19,3 @@ def run_interval():
 def remove_jobid():
     scheduler = BlockingScheduler()
     scheduler.remove('test')
-
-# from apscheduler.schedulers.blocking import BlockingScheduler
-# import datetime
-# import logging
-# logging.basicConfig(level=logging.INFO,format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s',datefmt='%Y-%m-%d %H:%M:%S',filename='log1.txt',filemode='a')
-# def aps_test(x):
-#     print datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
-#     def aps_date(x):
-#         scheduler = BlockingScheduler()
-#         scheduler.remove_job('interval_task')
-#         print datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
-#         scheduler = BlockingScheduler()
-#         scheduler.add_job(func=aps_test, args=('定时任务',), trigger='cron', second='*/5', id='cron_task')
-#         scheduler.add_job(func=aps_date, args=('一次性任务,删除循环任务',), next_run_time=datetime.datetime.now() + datetime.timedelta(seconds=12), id='date_task')
-#         scheduler.add_job(func=aps_test, args=('循环任务',), trigger='interval', seconds=3, id='interval_task')
-#         scheduler._logger = logging
-#         scheduler.start()
